98dfc0512bf0868d5675aaf4680c86e0f58efb29
PrivacySecured <LoveNewfies@proton.me>
2024-12-10 18:31:53 -0600

    Update script.js

--- redirectLoop/script.js
+++ redirectLoop/script.js
@@ -8,6 +8,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     function redirector() {
         const SITEURL = "https://gmt.gvnx.xyz/redirectLoop"; // Replace with your site URL
         const RNG = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
-        window.location.href = `${SITEURL}${RNG}`; // Redirect to SITEURL/RNG
+        window.location.href = `${SITEURL}/${RNG}`; // Redirect to SITEURL/RNG
     }
 });
