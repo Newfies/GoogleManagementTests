a5dcdcfd4f463a1e805d46919895cd63c351a27f
PrivacySecured <LoveNewfies@proton.me>
2024-12-10 18:34:32 -0600

    d

--- rdr/script.js
+++ rdr/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }, 3000); // 3000 milliseconds = 3 seconds
 
     function redirector() {
-        const SITEURL = "https://gmt.gvnx.xyz/redirectLoop"; // Replace with your site URL
+        const SITEURL = "https://gmt.gvnx.xyz/rdr"; // Replace with your site URL
         const RNG = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
         window.location.href = `${SITEURL}/${RNG}`; // Redirect to SITEURL/RNG
     }
