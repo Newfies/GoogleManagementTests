52275c65af4edd2c8a1f371bff5135746773f8f2
Newfie <157941859+Newfies@users.noreply.github.com>
2024-12-11 07:34:08 -0600

    Update script.js

--- rdr/script.js
+++ rdr/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
-    document.title = "TESTING ANALYTICS";
+    const randomtitlestring = Math.floor(Math.random() * 1000000000);
+    document.title = `[Loop Redirect Process] - ${randomtitlestring}`;
 
     // Wait 3 seconds, then run redirector()
     setTimeout(() => {
@@ -8,9 +9,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }, 3000); // 3000 milliseconds = 3 seconds
 
     function redirector() {
-        const SITEURL = "https://gmt.gvnx.xyz/rdr"; // Replace with your site URL
+        const SITEURL = "https://gmt.gvnx.xyz/LRP"; // Replace with your site URL
         const RNG = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
         const QUERYS = Math.floor(Math.random() * 1000000000);
-        window.location.href = `${SITEURL}/${RNG}?randomfuckinquery=${QUERYS}`; // Redirect to SITEURL/RNG?randomfuckinquery=QUERY
+        window.location.href = `${SITEURL}/${RNG}?q=${QUERYS}`; // Redirect to SITEURL/RNG?q=QUERY
     }
 });
