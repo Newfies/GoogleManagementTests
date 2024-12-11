e43abfc74c4b9dd1ed34f12c181ac8b4fafee285
PrivacySecured <LoveNewfies@proton.me>
2024-12-10 19:18:11 -0600

    Update script.js

--- rdr/script.js
+++ rdr/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         redirector();
     }, 3000); // 3000 milliseconds = 3 seconds
 
-    function redirector() {\
+    function redirector() {
         const SITEURL = "https://gmt.gvnx.xyz/rdr"; // Replace with your site URL
         const RNG = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
         const QUERYS = Math.floor(Math.random() * 1000000000);
