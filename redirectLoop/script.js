01845c4f6ce1be9e868dc326de18160f7c84dd9a
PrivacySecured <LoveNewfies@proton.me>
2024-12-10 18:23:48 -0600

    Update script.js

--- redirectLoop/script.js
+++ redirectLoop/script.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    const SITEURL = "https://gmt.gvnx.xyz/redirectLoop/"; // Replace with your site URL
-    const RNG = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
-    window.location.href = `${SITEURL}/${RNG}`; // Redirect to SITEURL/RNG
+
+    // Wait 3 seconds, then run redirector()
+    setTimeout(() => {
+        redirector();
+    }, 3000); // 3000 milliseconds = 3 seconds
+
+    function redirector() {
+        const SITEURL = "https://gmt.gvnx.xyz/redirectLoop"; // Replace with your site URL
+        const RNG = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
+        window.location.href = `${SITEURL}${RNG}`; // Redirect to SITEURL/RNG
+    }
 });
